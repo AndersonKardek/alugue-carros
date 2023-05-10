9e893b7ec1308e4876b2c4885964e1523b2dbc1c
Anderson <kardek.anderson@gmail.com>
2023-05-10 09:34:30 -0400

    feat: initilizing pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import light from "./styles/themes/light";
 
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import {Outlet} from "react-router-dom"
+
+import Home from "./routes/Home";
+
 
 function App() {
   return (
     <>
       <ThemeProvider theme={light}>
-        <GlobalStyle />
-        <Header />
-        <Hero />
+        <GlobalStyle /> 
+        <Home />
+        <Outlet />
       </ThemeProvider>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import Contact from "./routes/Contact.tsx";
+import About from "./routes/About.tsx";
+import ErrorPage from "./routes/ErrorPage.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path:"contact",
+        element: <Contact />
+      },
+      {
+        path:"about",
+        element: <About />
+      }
+    ]
+  }
+  
+])
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>
 );
