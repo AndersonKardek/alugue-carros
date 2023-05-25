70eee9d71a9bf205441b0172371d6f7d27ba562e
Anderson Kardek <kardek.anderson@gmail.com>
2023-05-25 19:58:20 -0400

    feat: inicializing PlanYourTrip

---
+++ src/components/PlanYourTrip/index.tsx
@@ -0,0 +1,10 @@
+const PlanYourTrip = () => {
+  return (
+    <section>
+      <h2>Planeje sua viagem agora</h2>
+      <h2>Rápido e Fácil aluguel de carro</h2>
+    </section>
+  );
+};
+
+export default PlanYourTrip;
